432e95f90e5541c2b55b0ee03ed672e99c69a0ec
Bittaurus <bittaurus@denied.win>
2021-05-30 14:59:34 -0700

    Added color to tiles and started build_map to create a terminal printable string of map.

--- src/game/map.rs
+++ src/game/map.rs
@@ -16,11 +16,13 @@ pub type Map = Vec<Vec<Tile>>;
 
 /// Map is created with Tile by Tile
 /// Each Tile has character to display, blocked, visited to display partial map
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub struct Tile {
     pub print: char,
     pub blocked: bool,
     pub visited: bool,
+    pub color: String,
+    pub print_colored: ColoredString,
 }
 
 /// Constructor for Tile struct
@@ -31,27 +33,45 @@ impl Tile {
             print: '.',
             blocked: false,
             visited: false,
+            color: "green".to_string(),
+            print_colored: '.'.to_string().color("green"),
         }
     }
 
+/*
+    // dead_code
     /// Creating wall Tile
     pub fn wall() -> Self {
         Tile {
             print: 'W',
             blocked: true,
             visited: false,
+            color: "red".to_string(),
+            print_colored: 'W'.to_string().color("red"),
         }
     }
+*/
 
+    pub fn to_wall(&mut self) {
+        self.print = 'W';
+        self.blocked = true;
+        self.visited = false;
+        self.color = "red".to_string();
+        self.print_colored = 'W'.to_string().color("red");
+    }
+
+/*
+    // dead_code
     /// Printing self
-    #[allow(dead_code)]
     pub fn print(&self) {
         print!("{}", self.print);
     }
+*/
 }
 
+/*
+// dead_code
 /// Takes in Height and Width of the map and creates a map then returns it
-#[allow(dead_code)]
 pub fn make_map(x: u64, y: u64) -> Map {
     let map = vec![vec![Tile::empty(); x as usize]; y as usize];
 
@@ -59,9 +79,11 @@ pub fn make_map(x: u64, y: u64) -> Map {
 
     map
 }
+*/
 
+/*
+// dead_code
 /// Function that takes in Map type and prints the base map of it
-#[allow(dead_code)]
 pub fn print_map(map: &Map) {
     // Iterate through map and print wall with red and empty with green
     for x in map.iter() {
@@ -77,10 +99,55 @@ pub fn print_map(map: &Map) {
         println!();
     }
 }
+*/
+
+/// Build a terminal printable map from map vector and overlay objects
+/// from object vector.
+//pub fn build_map(map: Vec<Vec<Tile>>, object: Vec<Object>) -> ColoredString {
+// just build from map vector until object vector is finished
+pub fn build_map(map: Vec<Vec<Tile>>) -> String {
+    #[derive(Clone, Debug)]
+    struct ColoredCell {
+        print_colored: ColoredString,
+    }
+
+    impl ColoredCell {
+        fn new() -> Self {
+            ColoredCell { print_colored: " ".to_string().color("white") }
+        }
+    }
+
+    let mut colormap = vec![vec![ColoredCell::new(); 0]; 0];
+
+    // build colormap from map
+    for outer in map {
+        let mut cells: Vec<ColoredCell> = Vec::new();
+        for inner in outer {
+            let mut cell = ColoredCell::new();
+            cell.print_colored = inner.print_colored;
+            cells.push(cell);
+        }
+        colormap.push(cells);
+    }
+
+    // overlay objects
+    // todo!();
+
+    // deconstruct colormap and build result
+    let mut result: String = "".to_string();
+    for outer in colormap {
+        for inner in outer {
+            result = format!("{}{}", inner.print_colored, result);
+        }
+        result = format!("{}\r\n", result);
+    }
+
+    result
+}
+
 
 /// Function that takes in file name and create map from read in text file
 /// Returns the created map
-#[allow(dead_code)]
 pub fn read_in_map(name: &str) -> Map {
     // Read in the file in string form
     let maptext = fs::read_to_string(name).expect("Something went wrong reading the file");
@@ -97,21 +164,28 @@ pub fn read_in_map(name: &str) -> Map {
 
     // Iterate through string and modify the all empty map
     for c in maptext.chars() {
-        if c == '1' {
-            map[i][j] = Tile::wall();
+       // wall
+       if "1-|+┌┘└┐┴┬├┤─│┼".contains(c) {
+            map[i][j].to_wall();
+            map[i][j].print = c;
             j += 1;
-        } else if c == '\n' {
-            i += 1;
-            j = 0;
-        } else {
-            j += 1;
-        }
+       // water
+       } else if "~%".contains(c) {
+           map[i][j].to_wall();
+           map[i][j].print = c;
+           map[i][j].color = "blue".to_string();
+           map[i][j].print_color = c.to_string().color("blue");
+       } else if c == '\n' {
+           i += 1;
+           j = 0;
+       } else {
+           j += 1;
+       }
     }
 
     map
 }
 
-#[allow(dead_code)]
 pub fn get_row_col(map: &Map) -> (u64, u64) {
     (map.len() as u64, map[0].len() as u64)
 }
